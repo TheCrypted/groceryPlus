d381d0d9fa4f49ae629f6b9208df870e7a5c58f4
Aman Sharma <mail.aman@protonmail.com>
2023-06-20 02:02:30 +0400

    next page autonavigation added

--- utils/indeedPull.cjs
+++ utils/indeedPull.cjs
@@ -12,33 +12,75 @@ async function indeedPullFunc(skill, location){
         timeout: 0,
         waitUntil: "networkidle0"
     })
-    const jobData = await page.evaluate(async (data) => {
-        let pagesInit = document.getElementsByClassName("e8ju0x50")
-        let pages = Array.from(pagesInit).slice(1,-1)
-        console.log(pages.length)
-        for (const pageRef of pages) {
-            await page.goto(pageRef.href, {
-                timeout: 0,
-                waitUntil: "networkidle0"
-            })
-            let jobsInit = document.getElementsByClassName("resultContent")
-            let jobs = Array.from(jobsInit)
-            jobs.forEach((job, index)=>{
-                let title = job.querySelector("h2.jobTitle > a > span") && job.querySelector("h2.jobTitle > a > span").innerText;
-                let href = job.querySelector("h2.jobTitle > a") && job.querySelector("h2.jobTitle > a").href;
-                let salary = job.querySelector("div.metadata.attribute_snippet") && job.querySelector("div.metadata.attribute_snippet").innerText
-                if (!salary) {
-                    salary = "Not given"
-                }
-                data.list.push({
-                    title: title,
-                    salary: salary,
-                    href: href
+    let origLink = `https://ae.indeed.com/jobs?q=${skill}&l=${location}&vjk=3787a18c0292a40b`
+    let hasNextPage = true;
+    let i = 0
+    while (hasNextPage && i < 10){
+        await page.goto(origLink, {
+            timeout: 0,
+            waitUntil: "networkidle0"
+        })
+        const jobData = await page.evaluate(async (data) => {
+            for (const pageRef of pages) {
+                await page.goto(pageRef.href, {
+                    timeout: 0,
+                    waitUntil: "networkidle0"
                 })
-            })
+                let jobsInit = document.getElementsByClassName("resultContent")
+                let jobs = Array.from(jobsInit)
+                jobs.forEach((job, index)=>{
+                    let title = job.querySelector("h2.jobTitle > a > span") && job.querySelector("h2.jobTitle > a > span").innerText;
+                    let href = job.querySelector("h2.jobTitle > a") && job.querySelector("h2.jobTitle > a").href;
+                    let salary = job.querySelector("div.metadata.attribute_snippet") && job.querySelector("div.metadata.attribute_snippet").innerText
+                    if (!salary) {
+                        salary = "Not given"
+                    }
+                    data.list.push({
+                        title: title,
+                        salary: salary,
+                        href: href
+                    })
+                })
+            }
+            return data
+        }, data)
+
+        const newLink = await page.evaluate(async()=>{
+            let pagesInit = document.querySelector("#jobsearch-JapanPage > div > div > div.jobsearch-SerpMainContent > div.jobsearch-LeftPane > nav > div:nth-child(7) > a")
+            return pagesInit?.href ?? null;
+        })
+        if(!newLink){
+            hasNextPage = false
         }
-        return data
-    }, data)
+        i++
+    }
+    // const jobData = await page.evaluate(async (data) => {
+    //     let pagesInit = document.getElementsByClassName("e8ju0x50")
+    //     let pages = Array.from(pagesInit).slice(1,-1)
+    //     console.log(pages.length)
+    //     for (const pageRef of pages) {
+    //         await page.goto(pageRef.href, {
+    //             timeout: 0,
+    //             waitUntil: "networkidle0"
+    //         })
+    //         let jobsInit = document.getElementsByClassName("resultContent")
+    //         let jobs = Array.from(jobsInit)
+    //         jobs.forEach((job, index)=>{
+    //             let title = job.querySelector("h2.jobTitle > a > span") && job.querySelector("h2.jobTitle > a > span").innerText;
+    //             let href = job.querySelector("h2.jobTitle > a") && job.querySelector("h2.jobTitle > a").href;
+    //             let salary = job.querySelector("div.metadata.attribute_snippet") && job.querySelector("div.metadata.attribute_snippet").innerText
+    //             if (!salary) {
+    //                 salary = "Not given"
+    //             }
+    //             data.list.push({
+    //                 title: title,
+    //                 salary: salary,
+    //                 href: href
+    //             })
+    //         })
+    //     }
+    //     return data
+    // }, data)
 
     console.log(`Successfully collected ${jobData.list.length} opportunities`)
     await browser.close();
