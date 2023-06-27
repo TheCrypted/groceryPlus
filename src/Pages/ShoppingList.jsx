596518d45efdedcff8d6d48b34e39c1c88914222
Aman Sharma <mail.aman@protonmail.com>
2023-06-27 16:21:10 +0400

    form is expansive and populated with items, added ability to query items within list form, changed front end elements for *aesthetic*

--- src/Pages/ShoppingList.jsx
+++ src/Pages/ShoppingList.jsx
@@ -8,11 +8,21 @@ import {AddCircleRounded} from "@mui/icons-material";
 export const ShoppingList = () => {
 	const [logState, setLogState] = useState(false)
 	const [user, setUser] = useState(null)
-	const [newList, setNewList] = useState(false)
+	const [items, setItems] = useState([])
+	const [heightState, setHeightState] = useState("33%")
+	const [showForm, setShowForm] = useState(false)
 	const navigate = useNavigate()
 	let newRef = useRef()
 	let showSelect = useRef()
+	const searchRef = useRef()
 
+	useEffect( ()=>{
+		fetch("http://localhost:3030/api/v1/items")
+			.then(response => response.json())
+			.then(resp => setItems(resp.list))
+			.catch(err => {console.log(err)})
+		console.log("check")
+	}, [])
 	const authCheck = async () => {
 		let token = localStorage.getItem("token")
 		if (!token) {
@@ -35,7 +45,12 @@ export const ShoppingList = () => {
 			}
 		}
 	}
-
+	function closeForm(e){
+		e.preventDefault();
+		console.log("button clicked")
+		showSelect.current.style.display = "flex"
+		setHeightState("33%")
+	}
 	useEffect(() => {
 		authCheck().then(resp => {
 			setLogState(resp.loggedIn)
@@ -44,7 +59,6 @@ export const ShoppingList = () => {
 			}
 		})
 	}, [])
-	console.log(user)
 	return (
 		<>
 		<div className="w-full h-[7%] z-10">
@@ -96,7 +110,7 @@ export const ShoppingList = () => {
 					<div className="text-white font-bold pl-2 text-4xl">Total Cost: $345.6</div>
 				</div>
 			</div>
-			<div ref={newRef} style={{boxShadow: "inset 0px 0px 80px rgba(0, 0, 0, 0.2)"}} className="text-gray-400 overflow-y-hidden font-semibold text-xl  h-full w-full bg-translucentHover rounded-2xl shadow-2xl backdrop-blur-2xl transition-all  "  onMouseEnter={()=>{
+			<div ref={newRef} style={{boxShadow: "inset 0px 0px 80px rgba(0, 0, 0, 0.2)", height: heightState}} className="h-1/3 text-gray-400 overflow-y-hidden font-semibold text-xl w-full bg-translucentHover rounded-2xl shadow-2xl backdrop-blur-2xl transition-all  "  onMouseEnter={()=>{
 				newRef.current.style.boxShadow = "inset 0px 0px 80px rgba(0, 0, 0, 0.4)"
 				showSelect.current.classList.add("text-white")
 				showSelect.current.classList.remove("text-gray-400")
@@ -104,8 +118,11 @@ export const ShoppingList = () => {
 				newRef.current.style.boxShadow = "inset 0px 0px 80px rgba(0, 0, 0, 0.2)"
 				showSelect.current.classList.remove("text-white")
 				showSelect.current.classList.add("text-gray-400")
-			}}>
-				{/*<div ref={showSelect} className="h-[28vh] hover:cursor-pointer  w-full flex items-center justify-center top-[20%]"><AddCircleRounded fontSize="large" color="primary" className="mr-4"/> Add new Shopping List</div>*/}
+			}} >
+				<div onClick={()=>{
+					showSelect.current.style.display = "none"
+					setHeightState("135%")
+				}} ref={showSelect} className="h-[28vh] hover:cursor-pointer  w-full flex items-center justify-center top-[20%]"><AddCircleRounded fontSize="large" color="primary" className="mr-4"/> Add new Shopping List</div>
 				<div className="h-[60vh] w-full p-4">
 					<form className="h-full w-full">
 						<input placeholder="Enter List Name" type="text" className="h-[13%] mb-6 shadow-md transition rounded-md w-full bg-translucentDarken focus:bg-translucentHover text-white font-semibold text-3xl focus:outline-none pl-4 focus:shadow-xl"/>
@@ -118,9 +135,64 @@ export const ShoppingList = () => {
 								<input  className=" h-full w-[20%] shadow-md transition rounded-r-md w-full bg-translucentDarken focus:bg-translucentHover text-white font-semibold text-3xl focus:outline-none pl-4 focus:shadow-xl" placeholder="Custom time" type="text"/>
 							</div>
 						</div>
-						<div  style={{boxShadow: "inset 0px 0px 80px rgba(0, 0, 0, 0.2)"}} className="h-[94%] w-full mb-6 rounded-md bg-translucentDarken"></div>
+						<div style={{boxShadow: "inset 0px 0px 80px rgba(0, 0, 0, 0.2)"}} className="h-[150%] w-full mb-6 rounded-md bg-translucentDarken p-4 flex flex-wrap">
+							<form className="w-full h-[8%]" onSubmit={(e)=>{
+								e.preventDefault()
+								let query = searchRef.current.value;
+								fetch(`http://localhost:3030/api/v1/itemsquery?q=${query}`)
+									.then(response => response.json())
+									.then(resp => setItems(resp.list))
+									.catch(e => console.log(e))
+							}}>
+								<input ref={searchRef} placeholder="Search Items" className="w-[85%] h-full shadow-md rounded-l-md bg-translucentHover focus:outline-none pl-3 text-white text-2xl focus:shadow-xl transition" type="text" />
+								<button className="w-[15%] rounded-r-md bg-translucentDarken h-full pt-1 focus:shadow-xl border-l-2 border-gray-700">Search</button>
+							</form>
+							<div className="w-full h-[92%] pt-6 p-2 overflow-y-auto scrollbar flex flex-wrap gap-1">
+								{
+									items.map((item, index)=>{
+										let backgroundCol;
+										let colourMain;
+										if (item.storeID === "L") {
+											colourMain = "rgba(80, 40, 40, 1)"
+										} else if (item.storeID === "C") {
+											colourMain = "rgba(40, 40, 100, 1)"
+										} else if (item.storeID === "N"){
+											colourMain = "#B58B00"
+										}
+										let packet
+										if(item.quantity){
+											let pReg = /[Pp]/g
+											let lastWord = item.title.split(" ")
+											let lastWordA = lastWord[lastWord.length - 1]
+											packet = lastWordA.match(pReg) === null
+											let price = item.cost/item.quantity
+											let redComp = Math.min(price * 9000, 250)
+											backgroundCol = packet ? `rgba(${redComp},40,${255-redComp}, 1)` : "rgba(100, 100, 100)";
+										}
+										return (
+											<div key={index} className="w-[19.2%] hover:shadow-2xl mr-2 mb-3 transition-all bg-gray-800 h-3/5 bg-white rounded-xl hover:cursor-pointer grid grid-rows-[70%_30%]">
+											<img className="rounded-t-xl w-full h-full" src={item.href} alt={item.title}/>
+											<div
+												className="grid grid-cols-[70%_30%] bg-red-500 rounded-b-xl text-white flex justify-left items-center text-2xl font-bold">
+												<div style={{backgroundColor: colourMain}} className="rounded-bl-xl h-full w-full  text-white grid grid-rows-[65%_35%] justify-start items-center pl-4 pr-4 font-semibold text-white text-2xl">
+													<div>Large Pumpkin Red</div>
+													<div
+														className="text-base flex items-center text-gray-300">{item.quantity + "g · " + (item.cost*1000/item.quantity).toString().slice(0, 5) + " د.إ per kg"}</div>
+												</div>
+												{<div style={{backgroundColor: backgroundCol}} className="rounded-br-xl h-full w-full text-white flex flex-wrap justify-center items-center font-bold text-white text-2xl">
+													<p>3.85<small>د.إ</small></p></div>}
+											</div>
+										</div>)
+									})
+								}
+
+							</div>
+
+						</div>
 						<div className="w-full flex justify-center h-full gap-4">
-							<button className="rounded-full transition-shadow h-[10%] w-1/6 border-2 border-red-500 text-red-500 font-semibold">Clear Selection</button>
+							<button className="rounded-full transition-shadow h-[10%] w-1/6 border-2 border-red-500 text-red-500 font-semibold" onClick={(e)=>{
+								closeForm(e)
+							}}>Clear Selection</button>
 							<button type="submit" className="rounded-full  transition-shadow bg-blue-500 h-[10%] w-1/6  text-gray-800 font-bold">Create List</button>
 						</div>
 					</form>
